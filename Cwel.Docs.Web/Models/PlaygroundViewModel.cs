22adcb0c8555b0abdfe2925ec2371cd352ec1fa7
Declan Cook <declan.cook@cwideonline.com>
2017-10-06 11:40:21 +0100

    Fix bug with saving and versions

--- Cwel.Docs.Web/Models/PlaygroundViewModel.cs
+++ Cwel.Docs.Web/Models/PlaygroundViewModel.cs
@@ -1,10 +1,9 @@
-﻿
+
 namespace Cwel.Docs.Web.Models
 {
     public class PlaygroundViewModel
     {
         public PlayVersion Play { get; set; }
-        public int? Version { get; set; }
         public string Id { get; set; }
     }
 }
