0ea82789413a311bf7eef725c04776419715dfb8
Gareth James <Gareth.James@OMC.CWOM.CO.UK>
2017-10-11 09:30:56 +0100

    Gulp icon now displays with gulp-notify toast messages

--- gulp/lib/util/options.js
+++ gulp/lib/util/options.js
@@ -1,11 +1,12 @@
 const notify = require('gulp-notify');
+const path = require('path');
 
 module.exports = {
     plumber: function onError(err) {
         notify.onError({
             title: 'I am Error.',
             message: `There has been an error in [${err.plugin}]`,
-            appIcon: './img/gulp.png'
+            icon: path.join(__dirname, '../', '../', 'img/gulp.png'),
         })(err);
         this.emit('end');
     },
