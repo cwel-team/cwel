f2ce013a760f375f077505ae35e1eabd1720b1d3
Daniel Stuessy <daniel.stuessy@cwideonline.com>
2017-10-18 10:12:47 +0100

    Optimize svgs distributed for CWEL

--- gulp/tasks/dist/img.js
+++ gulp/tasks/dist/img.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp');
 const del = require('del');
-
+const svgmin = require('gulp-svgmin');
 
 // @internal
 gulp.task('cwel-dist-img', () => gulp.src('Cwel/src/**/*.svg')
+.pipe(svgmin())
 .pipe(gulp.dest('Cwel/dist/Cwel')));
 // @internal
 gulp.task('clean:cwel-dist-img', () => del(['Cwel/dist/Cwel/**/*.svg']));
