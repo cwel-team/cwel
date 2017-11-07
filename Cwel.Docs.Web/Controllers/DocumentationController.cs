13bbc8478fdf532181d98a20897d5810b9a5ecf6
Daniel Stuessy <daniel.stuessy@cwideonline.com>
2017-11-07 09:19:41 +0000

    Moving doc page routes to `/page/{type}/{name}`.
    Dynamicaly specify type of component for doc page routes

--- Cwel.Docs.Web/Controllers/DocumentationController.cs
+++ Cwel.Docs.Web/Controllers/DocumentationController.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Web.Mvc;
 
 namespace Cwel.Docs.Web.Controllers
@@ -10,21 +11,12 @@ namespace Cwel.Docs.Web.Controllers
         /// <summary>
         /// Documentation page for a component
         /// </summary>
+        /// <param name="type">Type of the component to display documentation for</param>
         /// <param name="name">Name of the component to display documentation for</param>
-        [Route("Component/{name}")]
-        public ActionResult Component(string name)
+        [Route("Page/{type}/{name}")]
+        public ActionResult Component(string type, string name)
         {
-            return View($"~/Cwel/Component/{name}/index.cshtml");
-        }
-
-        /// <summary>
-        /// Documentation page for a component
-        /// </summary>
-        /// <param name="name">Name of the pattern to display documentation for</param>
-        [Route("Pattern/{name}")]
-        public ActionResult Pattern(string name)
-        {
-            return View($"~/Cwel/Pattern/{name}/index.cshtml");
+            return View($"~/Cwel/{CultureInfo.CurrentCulture.TextInfo.ToTitleCase(type)}/{name}/index.cshtml");
         }
     }
 }
