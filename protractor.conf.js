61330ae23397ceabfc28fb27c6581064594b32c3
Daniel Stuessy <daniel.stuessy@cwideonline.com>
2017-11-29 15:10:46 +0000

    New testng reporter for e2e tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,6 @@
 /* global jasmine */
-const SpecReporter = require('jasmine-spec-reporter').SpecReporter; // eslint-disable-line import/no-extraneous-dependencies
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const TestngReporter = require('./gulp/lib/test/jasmine-testng-reporter');
 
 module.exports.config = {
     seleniumServerJar: './node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.5.3.jar',
@@ -12,6 +13,13 @@ module.exports.config = {
                 displayStacktrace: true,
             },
         }));
+
+        return browser.getCapabilities().then((capabilities) => {
+            jasmine.getEnv().addReporter(new TestngReporter({
+                browserName: capabilities.get('browserName'),
+                dumpPath: 'Cwel/.tmp/test/Test/e2e/report/testng.xml',
+            }));
+        });
     },
     jasmineNodeOpts: {
         print() { },
