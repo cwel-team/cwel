9ec611fd98b3b29943ba23838e89bfbd12f66f9f
Daniel Stuessy <daniel.stuessy@cwideonline.com>
2018-01-10 15:33:11 +0000

    Cleaning up gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,21 @@ require('gulp-task-loader')('gulp-tasks');
 gulp.task('build', done => gulpSequence('sandbox:build', 'docs:internal:build')(done));
 
 gulp.task('watch', ['build'], done => {
+    const notifyStyles = [
+        'display: none',
+        'z-index: 9999',
+        'position: fixed',
+        'top: 0',
+        'left: 0',
+        'width: 100%',
+        'margin: 0',
+        'padding: 10px',
+        'font-family: sans-serif',
+        'font-size: 12px',
+        'text-align: center',
+        'color: #fff',
+        'background-color: #2a2a2a',
+    ];
     const docsBs = browserSync.create().init({
         server: 'tmp/docs/internal',
         port: 3000,
@@ -20,21 +35,7 @@ gulp.task('watch', ['build'], done => {
             port: 3001,
         },
         notify: {
-            styles: [
-                'display: none',
-                'z-index: 9999',
-                'position: fixed',
-                'top: 0',
-                'left: 0',
-                'width: 100%',
-                'margin: 0',
-                'padding: 10px',
-                'font-family: sans-serif',
-                'font-size: 12px',
-                'text-align: center',
-                'color: #fff',
-                'background-color: #2a2a2a',
-            ],
+            styles: notifyStyles,
         },
     });
     const sandboxBs = browserSync.create().init({
@@ -44,21 +45,7 @@ gulp.task('watch', ['build'], done => {
             port: 5001,
         },
         notify: {
-            styles: [
-                'display: none',
-                'z-index: 9999',
-                'position: fixed',
-                'top: 0',
-                'left: 0',
-                'width: 100%',
-                'margin: 0',
-                'padding: 10px',
-                'font-family: sans-serif',
-                'font-size: 12px',
-                'text-align: center',
-                'color: #fff',
-                'background-color: #2a2a2a',
-            ],
+            styles: notifyStyles,
         },
     });
 
@@ -78,13 +65,13 @@ gulp.task('watch', ['build'], done => {
 
 
 /*************************
- * Old gulp tasks
- ************************/
+* Old gulp tasks
+************************/
 
 
 /**
- * Display this help
- */
+* Display this help
+*/
 // gulp.task('help', doc.help());
 
 
@@ -93,43 +80,43 @@ gulp.task('watch', ['build'], done => {
 
 
 /**
- * Check the solution's code style.
- */
+* Check the solution's code style.
+*/
 // gulp.task('lint', done => gulpSequence('lint-script', 'lint-style')(done));
 
 
 /**
- * Perform analysis and generate reports.
- */
+* Perform analysis and generate reports.
+*/
 // gulp.task('analysis', done => gulpSequence('lint', 'cwel-analysis-cssstats-generate-data', 'cwel-analysis-cssstats-analyse-data')(done));
 
 
 /**
- * Build the whole project: packaging CWEL and generating docs.
- */
+* Build the whole project: packaging CWEL and generating docs.
+*/
 // gulp.task('build', done => gulpSequence('analysis', 'cwel-test-build', 'cwel-test-copy', 'cwel-dist', 'cwel-docs')(done));
 /**
- * Delete files created by build task
- */
+* Delete files created by build task
+*/
 // gulp.task('clean:build', done => gulpSequence('clean:cwel-dist', 'clean:cwel-docs')(done));
 
 
 /**
- * Run the front-end tests.
- */
+* Run the front-end tests.
+*/
 // gulp.task('test', ['cwel-dist', 'cwel-test-build', 'cwel-test-copy'], done => gulpSequence('cwel-test-run')(done));
 
 
 /**
- * Create CWEL blueprint files for a component, pattern or service:
- * e.g. Script, Razor, C# ViewModels, Test, Style, and Docs files
- */
+* Create CWEL blueprint files for a component, pattern or service:
+* e.g. Script, Razor, C# ViewModels, Test, Style, and Docs files
+*/
 // gulp.task('create', done => gulpSequence('cwel-create-duplo')(done));
 
 
 /**
- * Run MSBuild on this solution
- */
+* Run MSBuild on this solution
+*/
 // gulp.task('msbuild', () => gulp.src('./Cwel.sln')
 // .pipe(msbuild({
 //     toolsVersion: 'auto',
