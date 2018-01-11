ef70ebc849771f9af1122869a4252b592de8e4f7
Nathan Rodger <Nathan.Rodger@OMC.CWOM.CO.UK>
2018-01-11 13:55:42 +0000

    started adding grid spike with the billboard and the scroll animation spike

--- gulp-tasks/sandbox/script.js
+++ gulp-tasks/sandbox/script.js
@@ -2,12 +2,14 @@ const babel             = require('gulp-babel');                    // ES6 -> ES
 const del               = require('del');                           // Delete files and folders
 const concat            = require('gulp-concat');
 const gulp              = require('gulp');                          // Task automator
+//const gulpeach          = require('gulp-foreach');
 const gulpif            = require('gulp-if');                       // Conditionally run a task
 const path              = require('path');                          // Core NodeJS lib
 const plumber           = require('gulp-plumber');                  // Prevent errors from killing processes
 const process           = require('process');                       // Core NodeJS lib
 const rename            = require('gulp-rename');                   // Rename files
 const sourcemaps        = require('gulp-sourcemaps');               // Generate sourcemaps
+//const webpack           = require('webpack-stream');
 const yargs             = require('yargs');                         // Args
 
 const argv = yargs.argv; // Parse process.argv with yargs
@@ -22,6 +24,47 @@ const babelConfig = {
 };
 
 module.exports = () => del('tmp/sandbox/**/*.js');
+
+
+// module.exports = () => gulp.src([
+//     'sandbox/page/**/*.es',
+//     'cwel/component/**/*.es',
+//     'cwel/pattern/**/*.es',
+// ])
+// .pipe(gulpif(argv.chill, plumber(options.plumber)))
+// .pipe(gulpeach((stream, file) => {
+//     const p = path.parse(file.path);
+//     const tailPath = file.path
+//     .replace(file.base, '') // gulp base path
+//     .replace(p.base, ''); // filename
+//     const outputPath = path.resolve('tmp', 'sandbox', tailPath);
+//     const outputName = p.base.replace(p.ext, '.js');
+
+//     return stream
+//     .pipe(webpack({
+//         entry: file.path,
+//         output: {
+//             filename: outputName,
+//         },
+//         devtool: 'source-map',
+//         resolve: {
+//             extensions: ['.js', '.es'],
+//         },
+//         module: {
+//             rules: [
+//                 {
+//                     test: /\.es$/,
+//                     exclude: /(node_modules|Vendor)/,
+//                     use: {
+//                         loader: 'babel-loader',
+//                         options: babelConfig,
+//                     },
+//                 },
+//             ],
+//         },
+//     }))
+//     .pipe(gulp.dest(outputPath));
+// }));
 
 module.exports = () => gulp.src([
     'sandbox/page/**/*.es',
