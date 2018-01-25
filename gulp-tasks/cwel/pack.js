9de6ebd48ecd988699cf895ffb7a17f708a2a8ba
Daniel Stuessy <danielstuessy@gmail.com>
2018-01-25 08:48:01 +0000

    New gulp task to change package.json for npm publish

---
+++ gulp-tasks/cwel/pack.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const through = require('through2');
+
+// These paths are just there to stop gulp from loading the largest folders
+// into memory. The real authorities on which files get packaged are
+// .gitignore and .npmignore. They cover the rest when publishing
+// to npm from ~/tmp/package.
+module.exports = () => gulp.src([
+    './**',
+    '!node_modules/**',
+    '!.git/**',
+    '!tmp/**',
+    '!gulp-tasks/**',
+    '!gulp-lib/**',
+    '!gulp/**',
+    '!Docs/**',
+    '!Sandbox/**',
+], { dot: true })
+.pipe(through.obj((file, enc, cb) => {
+    if (file.path.indexOf('package.json') > -1) {
+        const data = JSON.parse(file.contents.toString());
+        delete data.scripts;
+        file.contents = Buffer.from(JSON.stringify(data));
+    }
+
+    cb(null, file);
+}))
+.pipe(gulp.dest('tmp/package'));
