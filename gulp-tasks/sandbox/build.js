ded41ee45e64461a3f53b8f5f3f336fb97446664
Daniel Stuessy <danielstuessy@gmail.com>
2018-02-01 13:42:43 +0000

    Clean gulp tasks for sandbox.

--- gulp-tasks/sandbox/build.js
+++ gulp-tasks/sandbox/build.js
@@ -1,9 +1,7 @@
 const gulpSequence     = require('gulp-sequence');
 
 module.exports = done => gulpSequence(
-    'sandbox:page-markup',
-    'sandbox:page-script',
-    'sandbox:page-style',
-    'sandbox:page-image',
-    'sandbox:cwel-script',
-    'sandbox:cwel-style')(done);
+    'sandbox:markup',
+    'sandbox:script',
+    'sandbox:style',
+    'sandbox:image')(done);
